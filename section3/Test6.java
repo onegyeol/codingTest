b9e8f91694adace76936ea59595766e3bfc8bde0
hangyeol choi <chlgksruf3414@gmail.com>
2023-11-10 16:04:46 +0900

    init

--- section3/Test6.java
+++ section3/Test6.java
@@ -6,6 +6,19 @@ public class Test6 {
     public int solution(int n, int k, int[] a){
         int answer=0;
 
+        for(int i=0; i<n; i++){
+            int sum = 0, cnt=0;
+            for(int j=i; j<n; j++){
+                if(a[j] == 0) {
+                    if(cnt == k) break;
+                    sum += 1;
+                    cnt++;
+                }
+                sum += a[j];
+            }
+            answer = Math.max(answer, sum);
+        }
+
         return answer;
     }
     public static void main(String[] args) {
@@ -16,7 +29,7 @@ public class Test6 {
         int k = in.nextInt();
         int[] arr = new int[n];
 
-        for(int i=0; i<k; i++){
+        for(int i=0; i<n; i++){
             arr[i] = in.nextInt();
         }
 
