534d258e31349cf5b9a8a325644a9c7365930ba1
hangyeol choi <chlgksruf3414@gmail.com>
2023-11-27 15:49:04 +0900

    프로그래머스 LV.2 짝지어 제거하기

---
+++ progammers/짝지어제거하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+//프로그래머스 lv.2
+class 짝지어제거하기
+{
+    public int solution(String s)
+    {
+        int answer=0;
+        Stack<Character> sta = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            if(sta.isEmpty()){
+                sta.push(s.charAt(i));
+            }
+            else{
+                if((sta.peek()).equals(s.charAt(i))) sta.pop();
+                else sta.push(s.charAt(i));
+            }
+            
+        }
+               
+        if(sta.isEmpty()) answer = 1;
+
+        return answer ;
+    }
+}
