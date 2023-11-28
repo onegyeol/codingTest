f99833acfa844071b60bc1b4556856e61979a910
hangyeol choi <chlgksruf3414@gmail.com>
2023-11-28 19:31:40 +0900

    코테 강의 4-5 k번째 큰 수 (TreeSet)

--- section4/Test5.java
+++ section4/Test5.java
@@ -5,7 +5,9 @@ import java.util.*;
 public class Test5 {
     public int solution(int n, int k, int[] a){
         int answer=-1;
-        TreeSet<Integer> tree = new TreeSet<>(Collections.reverseOrder());
+        //값을 찾지 못하면 -1 리턴해야하기에 초기값 -1로 지정
+        TreeSet<Integer> tree = new TreeSet<>(Collections.reverseOrder()); 
+        //디폴트가 오름차순이니까 reverseOrder 사용해서 내림차순으로
 
         for(int lt=0; lt<n-2; lt++){
             for(int i=lt+1; i<n-1; i++){
@@ -15,7 +17,7 @@ public class Test5 {
             }
         }
 
-        int cnt = 1;
+        int cnt = 1; //set은 인덱스로 접근 못해서 cnt로 따로 카운트 필요함.
         for(int i : tree){
             if(cnt == k){
                 answer = i;
