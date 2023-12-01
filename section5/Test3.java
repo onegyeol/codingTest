2bafc7ab2f95e6c91bd1ff1379d2ed45fbe7d891
hangyeol choi <chlgksruf3414@gmail.com>
2023-12-01 19:32:35 +0900

    코테 강의 5-3 크레인 인형 뽑기

--- section5/Test3.java
+++ section5/Test3.java
@@ -9,19 +9,19 @@ public class Test3 {
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(board[j][moves[i]-1] != 0 ){
-                    if(sta.isEmpty()) {
+                if(board[j][moves[i]-1] != 0 ){ //moves 값은 열을 의미하기에
+                    if(sta.isEmpty()) { //스택이 비어있으면 비교할 수 없기에 비어있다면 그냥 넣어줌
                         sta.push(board[j][moves[i]-1]); 
                     }
                     else if(sta.peek() == board[j][moves[i]-1]) {
-                        answer += 2;
-                        sta.pop();
+                        answer += 2; //같은 숫자니까 터뜨리고 +2 해줌
+                        sta.pop(); //해당 스택의 값 삭제
                     } 
                     else {
                         sta.push(board[j][moves[i]-1]);
                     }
-                    board[j][moves[i]-1] = 0;
-                    break;
+                    board[j][moves[i]-1] = 0; //해당 배열의 수 0으로 바꿔줌
+                    break; //안쪽 반복문 탈출
                 }
             }
         }
