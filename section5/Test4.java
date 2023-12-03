920a6c50245fcd35d36b2ecef71a787df6feb00e
hangyeol choi <chlgksruf3414@gmail.com>
2023-12-03 16:26:38 +0900

    코테 강의 5-4 후위식 연산

---
+++ section5/Test4.java
@@ -0,0 +1,32 @@
+package section5;
+
+import java.util.*;
+
+public class Test4{
+    public int solution(String s){
+        Stack<Integer> sta = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)) sta.push(Character.getNumericValue(c)); // char -> int 타입으로 바꿔서 스택에 넣어주기
+            else{
+                int second = sta.pop();
+                int first = sta.pop();
+                switch(c) {
+                    case '+' : sta.push(first+second); break;
+                    case '-' : sta.push(first-second); break;
+                    case '/' : sta.push(first/second); break;
+                    case '*' : sta.push(first*second); break;
+                }  
+            }
+        }
+        return sta.pop();
+    }
+    public static void main(String[] args) {
+        Test4 t = new Test4();
+        Scanner in = new Scanner(System.in);
+
+        String s = in.next();
+
+        System.out.println(t.solution(s));
+    }
+}
