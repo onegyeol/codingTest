c4acdb6c7f94f04e077b2bf820b7e8eff4eed312
onegyeol <chlgksruf3414@gmail.com>
2023-12-14 09:37:16 +0900

    코테 강의 6-3 선택 정렬

--- section6/Test3.java
+++ section6/Test3.java
@@ -1,5 +1,5 @@
 package section6;
-
+// 선택 정렬: 중첩 반복문 통해 매회마다 가장 작은값 앞으로
 import java.util.*;
 
 public class Test3 {
@@ -9,8 +9,8 @@ public class Test3 {
         for(int i=0; i<n-1; i++){
             least = i;
             for(int j=i+1; j<n; j++)
-                if(a[least] > a[j]) least = j;
-            int tmp = a[i];
+                if(a[least] > a[j]) least = j; //젤 앞의 값이 크다면 ? 가장 작은값이 아니기에 least 값 j로 변경
+            int tmp = a[i]; //swap 함수 구현
             a[i] = a[least];
             a[least] = tmp;
         }
