2c7062378b798f24532e0d619bd44c36ee1f567b
onegyeol <chlgksruf3414@gmail.com>
2023-12-30 17:46:31 +0900

    코테 강의 6-4 Least Recently Used

--- section6/Test4.java
+++ section6/Test4.java
@@ -8,19 +8,19 @@ public class Test4 {
 
         for(int x : works){
             int pos = -1;
-            for(int i=0; i<s; i++) if(x == cache[i]) pos = i;
-            if(pos == -1){
-                for(int i=s-1; i>=1; i--){
+            for(int i=0; i<s; i++) if(x == cache[i]) pos = i; //캐시 배열 안에 일치하는 값이 있으면 pos에 저장
+            if(pos == -1){ //cache miss
+                for(int i=s-1; i>=1; i--){ //처음부터 끝까지 배열 한칸씩 뒤로 미룸
                     cache[i] = cache[i-1];
                 }  
             }
-            else{
-                for(int i=pos; i>=1; i--){
+            else{ //cache hit
+                for(int i=pos; i>=1; i--){ //0~pos위치 앞까지 한칸씩 뒤로 미룸
                     cache[i] = cache[i-1];
                 }
                 
             }
-            cache[0] = x;
+            cache[0] = x; //캐시 모든 작업은 항상 제일 앞에서 이루어짐
         }
 
         return cache;
