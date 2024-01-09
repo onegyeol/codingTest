9579edbcdfe2215fb1eaa8ec6b0892e4390af99e
onegyeol <chlgksruf3414@gmail.com>
2024-01-09 23:55:05 +0900

    코테 강의 6-8 이분검색

---
+++ section6/Test8.java
@@ -0,0 +1,11 @@
+package section6;
+
+import java.util.*;
+
+public class Test8 {
+    public static void main(String[] args) {
+        Test8 t = new Test8();
+        Scanner in = new Scanner(System.in);
+
+    }
+}
