9122731cea3669b944158ec331e3b0e916002f12
onegyeol <chlgksruf3414@gmail.com>
2024-02-16 15:26:08 +0900

    코테강의 9-1. 씨름선수

--- section9/Test1.java
+++ section9/Test1.java
@@ -3,6 +3,7 @@ package section9;
 import java.util.*;
 
 /**
+ * 코테 강의 9-1. 씨름 선수 (그리디 알고리즘)
  * 현수는 씨름 감독입니다. 현수는 씨름 선수를 선발공고를 냈고, N명의 지원자가 지원을 했습니다.
 
 현수는 각 지원자의 키와 몸무게 정보를 알고 있습니다.
@@ -13,33 +14,49 @@ import java.util.*;
 
 존재하면 A지원자는 탈락하고, 그렇지 않으면 선발된다.”
 
-
  */
 
+class Body implements Comparable<Body> { //비교를 위해 클래스를 하나 더 만들어줌. Comparable 인터페이스 구현
+    public int h, w;
+    Body(int h, int w){
+        this.h = h;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Body o) { //키를 기준으로 정렬
+        return o.h-this.h;
+    }
+}
+
 public class Test1 {
+    public int solution(ArrayList<Body> a, int n){
+        int cnt=0;
+        Collections.sort(a); //일단 키를 기준으로 정렬
+        int max = Integer.MIN_VALUE;
+        for(Body b : a){ 
+            if(b.w > max){ //키로 정렬했기에 몸무게로만 비교하면 됨.
+                max = b.w;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
     public static void main(String[] args) {
         Test1 t = new Test1();
         Scanner kb = new Scanner(System.in);
-        int answer=0;
-
+        
         int n = kb.nextInt();
-        int[][] player = new int[n][2];
+        ArrayList<Body> arr = new ArrayList<>();
 
         for(int i=0; i<n; i++){
-            player[i][0] = kb.nextInt();
-            player[i][1] = kb.nextInt();
+            int h = kb.nextInt();
+            int w = kb.nextInt();
+            arr.add(new Body(h, w));
         }
 
-        for(int i=0; i<n; i++){
-            int cnt=0;
-            for(int j=0; j<n; j++){
-                if(i == j) continue;
-                else if(player[i][0] > player[j][0] || player[i][1] > player[j][1]) cnt++;
-            }
-            if(cnt == n-1) answer++;
-        }
+        System.out.println(t.solution(arr, n));
 
-        System.out.println(answer);
 
     }
 }
