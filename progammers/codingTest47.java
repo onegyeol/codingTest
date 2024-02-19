f31bca47af4fb5e33e8fb304038726f3225665c8
onegyeol <chlgksruf3414@gmail.com>
2024-02-19 15:57:55 +0900

    프로그래머스 Lv.2 구명보트

---
+++ progammers/codingTest47.java
@@ -0,0 +1,56 @@
+package progammers;
+
+import java.util.Arrays;
+
+/**프로그래머스 Lv.2 구명보트
+ * 
+ * 무인도에 갇힌 사람들을 구명보트를 이용하여 구출하려고 합니다. 
+ * 구명보트는 작아서 한 번에 최대 2명씩 밖에 탈 수 없고, 무게 제한도 있습니다.
+
+예를 들어, 사람들의 몸무게가 [70kg, 50kg, 80kg, 50kg]이고 구명보트의 무게 제한이 100kg이라면 
+2번째 사람과 4번째 사람은 같이 탈 수 있지만 1번째 사람과 3번째 사람의 무게의 합은 150kg이므로 구명보트의 무게 제한을 초과하여 같이 탈 수 없습니다.
+
+구명보트를 최대한 적게 사용하여 모든 사람을 구출하려고 합니다.
+
+사람들의 몸무게를 담은 배열 people과 구명보트의 무게 제한 limit가 매개변수로 주어질 때, 
+모든 사람을 구출하기 위해 필요한 구명보트 개수의 최솟값을 return 하도록 solution 함수를 작성해주세요.
+ */
+
+public class codingTest47 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int left=0;
+        int right=people.length-1;
+        //구명보트에 최대 두명밖에 탈 수 없는 것을 이용함
+        
+        Arrays.sort(people);
+        
+        while(left<right){
+            int sum = people[left]+people[right];
+            
+            if(sum > limit) right--;
+            else {
+                left++;
+                right--;
+            }
+            answer++;
+        }
+        
+        if(left == right) answer++;
+        
+        return answer;
+    }
+
+    /** 다른사람 풀이
+     * 
+     *  public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
+        }
+        return people.length - i;
+    }
+     */
+}
