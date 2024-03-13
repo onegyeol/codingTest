b262f56637451695339d33aacf0cf52ff1a789eb
onegyeol <chlgksruf3414@gmail.com>
2024-03-13 22:36:53 +0900

    코테 강의 10-5. 동전교환

---
+++ section10/Test5.java
@@ -0,0 +1,16 @@
+package section10;
+
+import java.util.*;
+
+public class Test5 {
+    static int[] coin;
+    public static void main(String[] args) {
+        Test5 t = new Test5();
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        coin = new int[n];
+
+        
+    }
+}
