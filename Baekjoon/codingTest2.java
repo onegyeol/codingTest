c92f3e2272f0f253eb4eaecee8edaa07feca9bdb
onegyeol <chlgksruf3414@gmail.com>
2024-03-20 13:37:42 +0900

    백준 2583 영역 구하기

---
+++ Baekjoon/codingTest2.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class codingTest2 {
+    public static void main(String[] args) {
+        codingTest2 t = new codingTest2();
+        Scanner in = new Scanner(System.in);
+
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        for(int i=0; i<k; i++){
+            int left_x = in.nextInt();
+            int left_y = in.nextInt();
+            int right_x = in.nextInt();
+            int right_y = in.nextInt();
+
+            
+        }
+    }
+}
