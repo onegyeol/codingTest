88218bc2a7d0d94104e26f9d351af62114f99332
onegyeol <chlgksruf3414@gmail.com>
2024-04-01 16:38:52 +0900

    백준 9019 DSLR

--- Baekjoon/codingTest8.java
+++ Baekjoon/codingTest8.java
@@ -3,43 +3,85 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+/**
+ * 백준 9019번 DSLR
+ * 
+ * 네 개의 명령어 D, S, L, R 을 이용하는 간단한 계산기가 있다. 이 계산기에는 레지스터가 하나 있는데,
+ *  이 레지스터에는 0 이상 10,000 미만의 십진수를 저장할 수 있다. 각 명령어는 이 레지스터에 저장된 n을 다음과 같이 변환한다. 
+ * n의 네 자릿수를 d1, d2, d3, d4라고 하자(즉 n = ((d1 × 10 + d2) × 10 + d3) × 10 + d4라고 하자)
+
+D: D 는 n을 두 배로 바꾼다. 결과 값이 9999 보다 큰 경우에는 10000 으로 나눈 나머지를 취한다. 그 결과 값(2n mod 10000)을 레지스터에 저장한다.
+S: S 는 n에서 1 을 뺀 결과 n-1을 레지스터에 저장한다. n이 0 이라면 9999 가 대신 레지스터에 저장된다.
+L: L 은 n의 각 자릿수를 왼편으로 회전시켜 그 결과를 레지스터에 저장한다. 이 연산이 끝나면 레지스터에 저장된 네 자릿수는 왼편부터 d2, d3, d4, d1이 된다.
+R: R 은 n의 각 자릿수를 오른편으로 회전시켜 그 결과를 레지스터에 저장한다. 이 연산이 끝나면 레지스터에 저장된 네 자릿수는 왼편부터 d4, d1, d2, d3이 된다.
+
+A에서 B로 변환하기 위해 필요한 최소한의 명령어 나열을 출력한다. 가능한 명령어 나열이 여러가지면, 아무거나 출력한다
+ */
 
 public class codingTest8 {
-    static int t;
-    static String[][] board;
-    static String[] answer, register;
-
-    static void BFS(int index, int cnt){
-        if((board[index][0]).equals(board[index][1])){
-            if(answer[index].length() > cnt) answer[index] = register[index];
-        }
-        else{
-            
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        t = br.read();
-        board = new String[t][2];
-        answer = new String[t];
-        register = new String[t];
-        
-        for(int i=0; i<t; i++){
-            String[] str = br.readLine().split(" ");
+        int t = Integer.parseInt(br.readLine());
 
-            board[i][0] = str[0];
-            board[i][1] = str[1];
-            BFS(0, 0);
+        /**
+         * while(t --> 0) 는 자바의 비교 연산자와 후위 감소 연산자를 함께 사용한 표현. while 루프에서 조건검사 시 사용
+           여기서 t는 루프의 반복 횟수를 나타내는 변수이며, -->는 후위 감소 연산자. 루프 마지막으로 가면 t는 -1씩 감소
+           이 연산자는 변수의 값을 감소시키고, 이전 값이 조건을 충족하는지 여부를 확인. 즉, t의 값이 0보다 큰 경우에만 루프가 실행됩니다.
+         */
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            Queue<Integer> q = new LinkedList<>();
+            boolean[] visited = new boolean[10000]; // BFS 탐색의 방문 여부 체크
+            String[] command = new String[10000]; // 정답 명령어를 담는 배열
+
+
+            q.add(a); // a를 큐에 담고
+            visited[a] = true; // 방문 표시한다
+            Arrays.fill(command,  ""); // 공백으로 초기화해줌
+
+
+            while (!q.isEmpty() && !visited[b]) {
+                int now = q.poll(); // 큐에서 값을 뺀다 = 현재 탐색 위치
+
+                int D = (2 * now) % 10000; // n을 두배로 바꾸고 10000으로 나눈 나머지
+                int S = now == 0 ? 9999 : now - 1; // 0일 때, 9999, 아니면 1을 빼준다 
+                int L = (now % 1000) * 10 + now / 1000; // 1234 -> 2341 : 1234를 1000으로 나눈 나머지(234)에 10을 곱함=2340, 1234를 1000으로 나누면 1, 2340+1=2341
+                int R = (now % 10) * 1000 + now / 10; // 1234 -> 4123 : 1234를 10으로 나눈 나머지에 1000 곱합 = 4000, 1234를 10으로 나누면 123, 4000+123=4123
+
+                if (!visited[D]) {
+                    q.add(D); // 큐에 넣는다
+                    visited[D] = true; // 방문처리한다
+                    command[D] = command[now] + "D"; // 명령어를 추가한다
+                }
+
+                if (!visited[S]) {
+                    q.add(S);
+                    visited[S] = true;
+                    command[S] = command[now] + "S";
+                }
+
+                if (!visited[L]) {
+                    q.add(L);
+                    visited[L] = true;
+                    command[L] = command[now] + "L";
+                }
+
+                if (!visited[R]) {
+                    q.add(R);
+                    visited[R] = true;
+                    command[R] = command[now] + "R";
+                }
+            }
+            System.out.println(command[b]);
         }
-
-
-
-        for(int i=0; i<t; i++){
-            System.out.println(answer[i]);
-        }
-
     }
 }
