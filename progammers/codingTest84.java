1bd09245d3391aa2dd4d950e4f6bc8d7a0072bd6
onegyeol <chlgksruf3414@gmail.com>
2024-06-29 21:05:49 +0900

    프로그래머스 Lv.2 방문 길이

---
+++ progammers/codingTest84.java
@@ -0,0 +1,32 @@
+package progammers;
+
+/**
+ * 프로그래머스 Lv.2 방문 길이
+ * 
+ * 게임 캐릭터를 4가지 명령어를 통해 움직이려 합니다. 명령어는 다음과 같습니다.
+
+U: 위쪽으로 한 칸 가기
+
+D: 아래쪽으로 한 칸 가기
+
+R: 오른쪽으로 한 칸 가기
+
+L: 왼쪽으로 한 칸 가기
+
+캐릭터는 좌표평면의 (0, 0) 위치에서 시작합니다. 
+좌표평면의 경계는 왼쪽 위(-5, 5), 왼쪽 아래(-5, -5), 오른쪽 위(5, 5), 오른쪽 아래(5, -5)로 이루어져 있습니다
+단, 좌표평면의 경계를 넘어가는 명령어는 무시합니다.
+ */
+
+public class codingTest84 {
+    public int solution(String dirs) {
+        int answer = 0;
+        int[][] board = new int[10][10];
+        int[][] visited = new int[10][10];
+        
+        for(int i=0; i<dirs.length(); i++){
+            
+        }
+        return answer;
+    }
+}
