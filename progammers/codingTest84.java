2cbbbaa229c5f4e46de098a4dad42f0f9dfd60e4
onegyeol <chlgksruf3414@gmail.com>
2024-06-30 19:03:15 +0900

    프로그래머스 Lv.2 방문길이

--- progammers/codingTest84.java
+++ progammers/codingTest84.java
@@ -1,5 +1,7 @@
 package progammers;
 
+import java.util.HashSet;
+
 /**
  * 프로그래머스 Lv.2 방문 길이
  * 
@@ -21,12 +23,51 @@ L: 왼쪽으로 한 칸 가기
 public class codingTest84 {
     public int solution(String dirs) {
         int answer = 0;
-        int[][] board = new int[10][10];
-        int[][] visited = new int[10][10];
-        
-        for(int i=0; i<dirs.length(); i++){
-            
+ 
+        HashSet<String> visited = new HashSet<>();
+
+        int currentX = 0;
+        int currentY = 0;
+ 
+        for (int i = 0; i < dirs.length(); i++) {
+            int nextX = currentX;
+            int nextY = currentY;
+            String moving = "";
+ 
+            if (dirs.charAt(i) == 'U') {
+                nextY++; 
+                moving += currentX + "" + currentY + "" + nextX +  nextY;
+/**
+ * 이렇게 적는 이유
+ * 
+ * 이미 방문한 곳을 다시 방문하지 않기 위해 이미 거쳐간 길을 중복처리 해준다.
+ * (0, 0) -> (1, 0) 0010
+ * (1, 0) -> (0, 0) 0010
+ * 
+ */
+
+            } else if (dirs.charAt(i) == 'D') {
+                nextY--;
+                moving += nextX + "" + nextY + "" + currentX + currentY ;
+            } else if (dirs.charAt(i) == 'R') {
+                nextX++; 
+                moving += currentX + "" + currentY + "" + nextX + nextY;
+            } else {
+                nextX--;
+                moving += nextX + "" + nextY + "" + currentX + currentY;
+            }
+ 
+            // 좌표평면의 경계를 넘어가면 추가하지 않고 다음 명령으로 넘어간다.
+            if (nextX < -5 || nextY < -5 || nextX > 5 || nextY > 5) {
+                continue;
+            }
+            visited.add(moving);
+ 
+            currentX = nextX;
+            currentY = nextY;
         }
+        answer = visited.size();
+ 
         return answer;
     }
 }
