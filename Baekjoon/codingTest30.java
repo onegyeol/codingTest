1ddedb456eea0dcdee8a7e9bd0231909ee600fef
onegyeol <chlgksruf3414@gmail.com>
2024-09-12 19:38:07 +0900

    백준 2096번 내려가기

---
+++ Baekjoon/codingTest30.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 백준 2096번 내려가기
+ * 
+ * N줄에 0 이상 9 이하의 숫자가 세 개씩 적혀 있다. 내려가기 게임을 하고 있는데, 이 게임은 첫 줄에서 시작해서 마지막 줄에서 끝나게 되는 놀이이다.
+먼저 처음에 적혀 있는 세 개의 숫자 중에서 하나를 골라서 시작하게 된다. 그리고 다음 줄로 내려가는데, 다음 줄로 내려갈 때에는 다음과 같은 제약 조건이 있다. 
+바로 아래의 수로 넘어가거나, 아니면 바로 아래의 수와 붙어 있는 수로만 이동할 수 있다는 것이다. 이 제약 조건을 그림으로 나타내어 보면 다음과 같다.
+ 숫자표가 주어져 있을 때, 얻을 수 있는 최대 점수, 최소 점수를 구하는 프로그램을 작성하시오. 점수는 원룡이가 위치한 곳의 수의 합이다.
+ */
+public class codingTest30 {
+    static int[] dx = {-1, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][3];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<3; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        
+    }
+}
