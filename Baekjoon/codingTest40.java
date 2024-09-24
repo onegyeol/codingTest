9fde73d564a975566bc671e4469f01d77565add4
onegyeol <chlgksruf3414@gmail.com>
2024-09-24 22:05:55 +0900

    백준 1339번 단어 수학

---
+++ Baekjoon/codingTest40.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * 백준 1339번 단어 수학
+ * 
+ * 민식이는 수학학원에서 단어 수학 문제를 푸는 숙제를 받았다.
+
+단어 수학 문제는 N개의 단어로 이루어져 있으며, 각 단어는 알파벳 대문자로만 이루어져 있다. 
+이때, 각 알파벳 대문자를 0부터 9까지의 숫자 중 하나로 바꿔서 N개의 수를 합하는 문제이다. 
+같은 알파벳은 같은 숫자로 바꿔야 하며, 두 개 이상의 알파벳이 같은 숫자로 바뀌어지면 안 된다.
+
+예를 들어, GCF + ACDEB를 계산한다고 할 때, A = 9, B = 4, C = 8, D = 6, E = 5, F = 3, G = 7로 결정한다면, 두 수의 합은 99437이 되어서 최대가 될 것이다.
+
+N개의 단어가 주어졌을 때, 그 수의 합을 최대로 만드는 프로그램을 작성하시오.
+ */
+public class codingTest40 {
+    static int n;
+    static String[] str;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        str = new String[n];
+
+        for(int i=0; i<n; i++){
+            str[i] = br.readLine();
+        }
+        
+    }
+}
