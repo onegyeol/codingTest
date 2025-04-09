b6be496078d982e16d5c03587b3094199f9bc68b
onegyeol <chlgksruf3414@gmail.com>
2025-04-09 21:26:56 +0900

    백준 3190번 뱀

--- Baekjoon/codingTest82.java
+++ Baekjoon/codingTest82.java
@@ -39,7 +39,7 @@ public class codingTest82 {
     static int[][] board;
     static HashMap<Integer, String> spins = new HashMap<>();
     static Deque<int[]> snake = new LinkedList();
-    static int[] dx = {0, 1, 0, -1};
+    static int[] dx = {0, 1, 0, -1}; //하, 좌, 상, 우
     static int[] dy = {1, 0, -1, 0};
 
     public static void main(String[] args) throws NumberFormatException, IOException {
