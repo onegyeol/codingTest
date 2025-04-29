178700d33073fafb5a192f7f8c20357ea01dee57
onegyeol <chlgksruf3414@gmail.com>
2025-04-29 13:02:54 +0900

    백준 11286번 절댓값 힙

---
+++ Baekjoon/codingTest84.java
@@ -0,0 +1,28 @@
+package Baekjoon;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+
+
+public class codingTest84 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        
+        for(int i=0; i<n; i++){
+            int tmp = in.nextInt();
+
+            if(tmp == 0){
+
+            }
+            else{
+
+            }
+        }
+    }
+}
