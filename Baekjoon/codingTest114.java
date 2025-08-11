6a4eaaf4dcb0e27149722eae4a5ac9494a6e58b8
hangyeol <chlgksruf3414@gmail.com>
2025-08-11 13:14:33 +0900

    백준 1100번 하얀 칸

--- Baekjoon/codingTest114.java
+++ Baekjoon/codingTest114.java
@@ -11,7 +11,7 @@ import java.io.InputStreamReader;
  * 가장 왼쪽 위칸 (0,0)은 하얀색이다. 체스판의 상태가 주어졌을 때, 하얀 칸 위에 말이 몇 개 있는지 출력하는 프로그램을 작성하시오.
  */
 
-public class coidngTest114 {
+public class codingTest114 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
