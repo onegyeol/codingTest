3464b6dca854e2fa1acbd927dd3fa0cc9389487c
hangyeol <chlgksruf3414@gmail.com>
2025-10-28 10:15:28 +0900

    프로그래머스 Lv.2 소수찾기

--- progammers/codingTest111.java
+++ progammers/codingTest111.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 
+/* 
+ * 프로그래머스 Lv.2 소수찾기
+ * 
+ * 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.ㄴ
+ * 
+ */
+
 public class codingTest111 {
     static HashSet<Integer> set = new HashSet<>();
     public static void main(String[] args) throws IOException {
